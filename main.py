81012a6aa8122bbf6aac16b26663947b4a40e6f5
Oudarja Barman Tanmoy <oudarja360@gmail.com>
2025-05-20 13:15:36 +0600

    First commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from dotenv import load_dotenv
+import os
+app = FastAPI()
+# Load environment variables from .env file
+load_dotenv()
+# When the root url is accessed then return of this message
+@app.get('/')
+def root():
+    secret_key = os.getenv('SECRET_KEY')
+    return {
+        "message": f"Secret key is: {secret_key}. I am learning GitHub Actions (Branch Protection, Automated Testing, Workflow creation, GitHub Secrets)"
+    }
---
+++ tests/test_main.py
@@ -0,0 +1,14 @@
+from fastapi.testclient import TestClient
+from main import app
+from dotenv import load_dotenv
+import os
+client = TestClient(app)
+# Load environment variables from .env file
+load_dotenv()
+def test_root():
+    response = client.get("/")
+    secret_key = os.getenv('SECRET_KEY')
+    assert response.status_code == 200
+    assert response.json() == {
+         "message": f"Secret key is: {secret_key}. I am learning GitHub Actions (Branch Protection, Automated Testing, Workflow creation, GitHub Secrets)"
+    }
