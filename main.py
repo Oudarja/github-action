cff057e856876b846641b4bc54f7e43f78946d62
Oudarja Barman Tanmoy <51699905+Oudarja@users.noreply.github.com>
2025-07-31 15:30:53 +0600

    My first commit: main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
+
 app = FastAPI()
-# Load environment variables from .env file
-load_dotenv()
-# When the root url is accessed then return of this message
+
+def load_environment_variables() -> None:
+    """
+    Loads environment variables from .env file.
+    """
+    load_dotenv()
+
+load_environment_variables()
+
 @app.get('/')
-def root():
-    secret_key = os.getenv('SECRET_KEY')
+def root() -> dict:
+    """
+    Returns a message with the secret key when the root URL is accessed.
+
+    Returns:
+        dict: A dictionary containing a message with the secret key.
+    """
+    secret_key: str = os.getenv('SECRET_KEY')
     return {
         "message": f"Secret key is: {secret_key}. I am learning GitHub Actions (Branch Protection, Automated Testing, Workflow creation, GitHub Secrets)"
     }
